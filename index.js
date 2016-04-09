f2d17c4b682783c72e249bb192a1c61f875fd20a
Isaac Plunkett <iplunkett@localhost.localdomain>
2016-04-09 10:21:56 -0700

    read from file to set up room.  respond to user input. and quit on command.

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 var prompt = require('prompt');
-var leverR = 'up';
-var leverL = 'up';
-
-console.log('There are two levers, one on the right, and the other on the left.');
+var fs = require('fs');
 
 prompt.start();
 
-printLevers();
-getInput();
+var description;
+
+var leverR;
+var leverL;
+
+//attempt to read from a file
+fs.readFile('/home/iplunkett/Documents/gamejam/gamejam_train/leverRoom.txt', 'utf-8', function(err, data){
+	if(err) throw err;
+	var str = data.split("\n");
+	description = str[0];
+	leverR = str[1];
+	leverL = str[2];
+	console.log(description);
+	printLevers();
+	//starts the main loop
+	getInput();
+});
 
 function getInput() {prompt.get(['command'], function (err, result){
+
+	//incase of an error
 	if(err) {return onErr(err);}
+	//quits the game
+	if(result.command === 'quit')
+	{return;}
+	
+	//flip one lever, restores the other lever
 	if(result.command === 'flip left lever')
 	{flipLeverL();}
 	if(result.command === 'flip right lever')
 	{flipLeverR();}
+	
+	
+	//prints the levers
 	printLevers();
+	//recursion to make an infinite loop
 	getInput();
 });
 }
