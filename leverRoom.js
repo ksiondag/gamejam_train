ad9a4365878078ebf509c5b612819e13036ac5c5
Isaac <plunkett5@gmail.com>
2016-04-09 14:48:20 -0700

    added leverRoom.js as a seperate file

---
+++ leverRoom.js
@@ -0,0 +1,53 @@
+var game = require('./game');
+var index = require('./index');
+
+var leverR;
+var leverL;
+
+//flip levers and save the state of the room
+function flipLever(str)
+{
+	if(str === 'left')
+	{
+		leverL= leverL ? false : true;
+		leverR=false;
+	}
+	if(str === 'right')
+	{
+		leverR= leverR ? false : true;
+		leverL=false;
+	}
+	
+	game.getRoom((room) => {
+        room.state['left lever'] = leverL;
+        room.state['right lever'] = leverR;
+
+        game.saveRoom(room, (err) => {if(err) throw err;});
+    });
+}
+
+//prints the state of the levers, true is up, false is down
+function printLevers()
+{
+	var str1 = leverL ? 'up' : 'down';
+	var str2 = leverR ? 'up' : 'down';
+	console.log('Left Lever: ' + str1 + ' Right Lever: '  + str2);
+}
+
+//make a new lever room
+function makeLever()
+{
+	//attempt to read from a file
+	game.getRoom((room) => {
+	leverR = room.state['right lever'];
+	leverL = room.state['left lever'];
+	
+	console.log(room.description);
+	
+	index.getInput();
+});
+}
+
+exports.makeLever = makeLever;
+exports.printLevers = printLevers;
+exports.flipLever = flipLever;
