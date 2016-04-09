a609562f948b251b000c64b35a2f12d616d2ee1c
Isaac Plunkett <iplunkett@localhost.localdomain>
2016-04-08 21:10:27 -0700

    get user input and flip levers

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 var prompt = require('prompt');
+var leverR = 'up';
+var leverL = 'up';
+
+console.log('There are two levers, one on the right, and the other on the left.');
 
 prompt.start();
 
-prompt.get(['command'], function (err, result){
+printLevers();
+getInput();
+
+function getInput() {prompt.get(['command'], function (err, result){
 	if(err) {return onErr(err);}
-	console.log('Command-line-input recieved:');
-	console.log(' Command: ' + result.command);
+	if(result.command === 'flip left lever')
+	{flipLeverL();}
+	if(result.command === 'flip right lever')
+	{flipLeverR();}
+	printLevers();
+	getInput();
 });
+}
 
 function onErr(err) {
 	console.log(err);
 	return 1;
+}
+
+function flipLeverR(){
+	leverR = 'down';
+	leverL = 'up';
+}
+
+function flipLeverL(){
+	leverL = 'down';
+	leverR = 'up';
+}
+
+function printLevers()
+{
+	console.log('Left Lever: ' + leverL + ' Right Lever: ' + leverR);
 }
