ea00094a74c3b26865c9d0a540cc0f0502554011
Silent K <silentksionda@gmail.com>
2016-04-09 22:57:26 -0700

    Basic functionality! Wow!

--- game.js
+++ game.js
@@ -4,59 +4,82 @@ Game logic and state.
 
 // Node libraries in use
 var fs = require('fs');
+var path = require('path');
 
 var extraFs = require('./extra-fs');
 
-var CURRENT_ROOM = 'saveState/currentRoom';
+var rooms = [
+    'leverRoom'
+];
 
 var initSaveState = (callback) => {
     extraFs.copyDir('assets', 'saveState', () => {
-        fs.symlink('leverRoom', CURRENT_ROOM, (err) => {
-            if (err) {
-                throw err;
-            }
-            fs.rename(CURRENT_ROOM + '/git', CURRENT_ROOM + '/.git', (err) => {
+        fs.writeFile('assets/currentRoom.txt', `${rooms[0]}\n`, 'utf-8', (err) => {
+            // TODO: make all the copied assets proper git directories
+            var root = 'saveState/leverRoom';
+            fs.rename(
+                path.join(root, '/git'), path.join(root, '/.git'), (err) => {
                 if (err) {
                     throw err;
                 }
                 getRoom(callback);
-            })
+            });
         });
     });
 };
 
-var getRoom = (callback) => {
-    fs.readFile(CURRENT_ROOM + '/info.json', 'utf-8', (err, data) => {
+var currentRoom = (callback) => {
+    fs.readFile('saveState/currentRoom.txt', 'utf-8', (err, data) => {
+        var roomString, remainingRooms;
         if (err) {
-            // Save state does not exist yet
-            initSaveState(callback);
-        } else {
-            callback(
-                JSON.parse(data)
-            );
+            return callback(err);
         }
+
+        roomString = data.trim();
+        remainingRooms = rooms.reverse();
+
+        while (remainingRooms.pop() !== roomString) {}
+
+        rooms = [roomString].concat(remainingRooms.reverse());
+
+        callback(null, roomString);
     });
 };
 
-var saveRoom = (room, callback) => {
-	fs.writeFile(CURRENT_ROOM + '/info.json', JSON.stringify(room, null, '\n\n'), callback);
+var getRoom = (callback) => {
+    currentRoom((err, roomString) => {
+        if (err) {
+            // Save state does not exist yet
+            return initSaveState(callback);
+        }
+        fs.readFile(path.join('saveState', roomString, 'info.json'), 'utf-8', (err, data) => {
+            if (err) {
+                throw err;
+            } else {
+                callback(
+                    JSON.parse(data)
+                );
+            }
+        });
+    });
 };
 
-var getCommand = (room, command) => {
-    var name;
-
-    if (room.commands.hasOwnProperty(command)) {
-        return command;
-    }
-
-    for (name in room.commands) {
-        if (room.commands[name].alias.indexOf(command) !== -1) {
-            return name;
+var passesWinCondition = (room) => {
+    var thing;
+    for (thing in room.winningState) {
+        if (room.winningState[thing] !== room.state[thing]) {
+            return false
         }
     }
+
+    console.log(room.winningText);
+
+    return true;
 };
 
 exports.getRoom = getRoom;
-exports.saveRoom = saveRoom;
-exports.getCommand = getCommand;
+exports.currentRoom = currentRoom;
+exports.passesWinCondition = passesWinCondition;
+//exports.saveRoom = saveRoom;
+//exports.getCommand = getCommand;
 
---
+++ main.js
@@ -0,0 +1,100 @@
+'use strict';
+var rl = require('readline').createInterface({
+    input: process.stdin, output: process.stdout
+});
+var path = require('path');
+
+var game = require('./game');
+
+var git = require('./git');
+
+var commands = {
+    help: function (room, callback) {
+        console.log(
+`You asked for help! Yay!
+
+Seriously, though, here are some commands:
+    help: prints this prompt
+    quit: exits the game
+    whereami: get some information on where you are
+    history: find out what's been done in this place
+    realities: find out what parallel realities exist
+    merge: merge another reality with the current one
+`
+        );
+        callback();
+    },
+    quit: function () {
+        rl.close();
+        process.exit(0);
+    },
+    whereami: function (room, callback) {
+        console.log(room.description);
+        callback();
+    },
+    history: function (room, callback) {
+        game.currentRoom((err, roomString) => {
+            git.log(path.join('saveState', roomString), (code, data) => {
+                console.log(`${data}`);
+                callback();
+            });
+        });
+    },
+    realities: function (room, callback) {
+        game.currentRoom((err, roomString) => {
+            git.branch(path.join('saveState', roomString), (code, data) => {
+                console.log(`${data}`);
+                callback();
+            });
+        });
+    },
+    merge: function (room, reality, callback) {
+        game.currentRoom((err, roomString) => {
+            git.merge(path.join('saveState', roomString), reality, (code, data) => {
+                console.log(`Reality ${reality} merged.`);
+                callback();
+            });
+        });
+    }
+}
+
+var prompts = [
+    'Enter help for more information: ',
+    'Enter command: '
+].reverse();
+
+var question;
+
+var main = () => {
+    game.getRoom((room) => {
+        var roomIndex;
+
+        if (game.passesWinCondition(room)) {
+            // TODO: print winning text
+            // TODO: update to next room
+        }
+
+        if (prompts.length > 0) {
+            question = prompts.pop();
+        }
+        rl.question(question, (result) => {
+            var args, name;
+
+            args = result.split(' ');
+
+            name = args[0];
+            args = [room].concat(args.slice(1), main);
+
+            if (commands.hasOwnProperty(name)) {
+                commands[name].apply(this, args);
+            } else {
+                console.log(`${result} is an unrecognized command`);
+                main();
+            }
+        });
+    });
+};
+
+if (require.main === module) {
+    main();
+}
